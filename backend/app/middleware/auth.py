6e1f6b526146a8305b304c8d17af8f21ac3a67e3
Priyank Khatri <priyank.khatri.cg@gmail.com>
2026-07-16 14:06:16 +0530

    update auth.py

--- backend/app/middleware/auth.py
+++ backend/app/middleware/auth.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from app.auth.jwt_handler import verify_token
 
-security = HTTPBearer()
+security = HTTPBeare()
 
 
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict:
