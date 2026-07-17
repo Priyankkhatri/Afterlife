125d401b62b5b1535dbc9294ee55817fa0f369d6
Priyank Khatri <priyank.khatri.cg@gmail.com>
2026-07-17 13:52:48 +0530

    Update connection.py

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
-
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "afterlife")
 
 
 client = MongoClient(MONGODB_URI)
